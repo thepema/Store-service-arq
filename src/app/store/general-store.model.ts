af5fb30e248c621fb0fdb79f810e3a4652a375c4
thepema <pema7284@gmail.com>
2025-11-19 18:26:19 +0100

    mejoras de tipado

--- src/app/store/general-store.model.ts
+++ src/app/store/general-store.model.ts
@@ -1,11 +1,6 @@
-import { Signal } from "@angular/core";
-import { Observable } from "rxjs";
+export type Prettify<T> = { [K in keyof T]: T[K] };
 
-type Prettify<T> = { [K in keyof T]: T[K] };
-
-export type Store<T> = Prettify<{ [K in keyof T]: T[K] }>;
+export type Store<T> = { [K in keyof T]: T[K] };
 export type Keys<T> = keyof T;
 export type Values<T> = T[keyof T];
-export type SubjectValue<T> = Store<T> | {};
-export type ResultSelect<T> = Observable<Prettify<{ [K in keyof T]: T[K]; }>[keyof T] | undefined>;
-export type ResultSelectSignal<T> = Signal<Prettify<{ [K in keyof T]: T[K]; }>[keyof T] | undefined>;
+export type SubjectValue<T> = Store<T> | object;
